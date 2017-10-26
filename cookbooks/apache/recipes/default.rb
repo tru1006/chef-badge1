5c266d57ca0b44c5619043e209161ddfef1cef03
Bobby <tru1006@yahoo.com>
2017-10-26 00:22:37 +0000

    changes

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -4,6 +4,13 @@
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
 #Insatll apache package
+
+if node['platform_family'] == "rhel"
+	package = "httpd"
+elsif node ['platform_family'] == "debian"
+	package = "apache2"
+end
+
 package 'apache2' do
 	package_name 'httpd'
 	action :install
@@ -13,5 +20,4 @@ service 'apache2' do
 	service_name 'httpd'
 	action [:start, :enable]
 end
-
 
